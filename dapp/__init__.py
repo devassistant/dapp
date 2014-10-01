16a686dbfca8af73d244d2c2e54bbf74f3052d4f
Slavek Kabrda <bkabrda@redhat.com>
2014-10-01 16:47:25 +0200

    Initial implementation of the protocol, still many TODOs and decisions to be made...

--- dapp/__init__.py
+++ dapp/__init__.py
@@ -1,3 +1,270 @@
 # -*- coding: utf-8 -*-
+# TODO: what to do if:
+#  1) there's unsupported protocol version on the other side? (fail?)
+#  2) message is not understood/malformed? (fail?/warn and continue?)
+#  3) client calls a non existing command runner? (should it be handled here or in DA itself?)
+import logging
+import sys
+
+import six
+import yaml
 
 __version__ = "0.0.1"
+
+class DAPPException(BaseException):
+    pass
+
+class DAPPCommunicator(object):
+    def __init__(self, protocol_version=__version__, logger=None):
+        self.protocol_version = protocol_version
+        self.logger=logger
+
+    def send_msg(self, msg_type, ctxt, data=None):
+        """Send a message to the other communicating side through the pipe.
+
+        Args:
+            ctxt: global variable context from Yaml DSL
+            msg_type: type of message (e.g. what this message means)
+            data: dict of extra data, depends on msg_type
+
+        Raises:
+            DAPPException if something goes wrong while message is being sent
+        """
+        raise NotImplementedError()
+
+    def recv_msg(self):
+        """Receive a message to the other communicating side through the pipe.
+
+        Return:
+            dict of decoded data sent through the pipe by the other side
+
+        Raises:
+            DAPPException if something goes wrong while message is being sent or
+                if the message is malformed
+        """
+        raise NotImplementedError()
+
+    def log(self, level, msg):
+        """Log a message at given level, assuming self.logger is set,
+        do nothing otherwise.
+        """
+        if self.logger:
+            self.logger.log(level, msg)
+
+    def _compose_msg(self, ctxt=None, data=None):
+        """Compose a message to send through the pipe.
+        Note: This method expects that we're not sending binary data. If someone ever needs
+        to do this (I don't think so, but...), this will have to be seriously rethought.
+
+        Args:
+            ctxt: global variable context from Yaml DSL
+            data: extra data to include in the message
+
+        Returns:
+            utf8 encoded message that can be sent through the pipe
+        """
+        msg = ['START']
+        msg.append('dapp_protocol_version: "{v}"'.format(v=self.protocol_version))
+        if ctxt is not None:
+            msg.append(self._dump_ctxt(ctxt).strip())
+        if data is not None:
+            if not isinstance(data, dict):
+                raise TypeError('msg must be a dict')
+            msg.append(yaml.dump(data, default_flow_style=False).strip())
+        msg.append('STOP\n')
+
+        # all parts of message should be unencoded at this point
+        return '\n'.join(msg).encode('utf8')
+
+    def _message_from_start_stop_list(self, lst):
+        """Check a list of lines of received message and return a string without
+        "START" and "STOP" and possibly empty lines in the beginning and end.
+
+        Args:
+            lst: list of lines of the message as read from the pipe, including
+                "START" and "STOP" and possibly empty lines before and after
+
+        Returns:
+            string containing the whole message without "START" and "STOP"
+
+        Raises:
+            DAPPException if the message is somehow malformed
+        """
+        start_from = -1
+        stop_at = -1
+        for i, l in enumerate(lst):
+            if l == 'START':
+                start_from = i + 1
+            elif start_from == -1 and l != '':
+                raise self._wrong_subprocess_msg_error(lst, 'Nonempty line before "START" keyword')
+            if l == 'STOP':
+                stop_at = i
+            elif stop_at != -1 and l != '':
+                raise self._wrong_subprocess_msg_error(lst, 'Nonempty line after "STOP" keyword')
+
+        if start_from == -1 and stop_at == -1:
+            # just empty lines, this is OK => return None
+            return None
+
+        # "start_from == -1" can't happen, because it would either be caught in the loop
+        #  if a non empty line was before it; otherwise both start_from and stop_at are -1
+        #  and that is handled in the condition above
+        if stop_at == -1:
+            raise self._wrong_subprocess_msg_error(lst, 'No "STOP" keyword found in message')
+
+        return '\n'.join(lst[start_from:stop_at])
+
+    def _wrong_subprocess_msg_error(self, msg_lst, extra=''):
+        """A helper function that formats malformed message errors with some extra info."""
+        if extra:
+            extra = ' ({0})'.format(extra)
+        err = ['Wrong DAPP message{0}:'.format(extra)]
+        err.extend(msg_lst)
+        return DAPPException('\n'.join(err))
+
+    def _dump_ctxt(self, ctxt):
+        """Dump a Yaml DSL context into dict.
+
+        Args:
+            ctxt: the whole Yaml DSL context
+
+        Returns:
+            dict with one key "ctxt" and value which contains the whole dumped context;
+            "__assistant__" is always omitted from the context
+        """
+        dumped = {'ctxt': {}}
+        for k, v in ctxt.items():
+                # exclude unwanted keys
+            if k not in ['__assistant__']:
+                dumped['ctxt'][k] = v
+        # we want to produce unencoded string here (e.g. unicode in py2, str in py3)
+        #  so we need to pass encoding=None
+        #  http://pyyaml.org/wiki/PyYAMLDocumentation#Python3support
+        return yaml.dump(dumped, encoding=None, default_flow_style=False)
+
+
+class DAPPServer(DAPPCommunicator):
+    def __init__(self, proc, protocol_version=__version__, logger=None):
+        super(DAPPServer, self).__init__(protocol_version, logger)
+        self.proc = proc
+
+    def send_msg(self, msg_type, ctxt=None, data=None):
+        # TODO: check msg_type and data
+        send_msg = {'msg_type': msg_type}
+        if data:
+            send_msg.update(data)
+        msg = self._compose_msg(ctxt, data=send_msg)
+        self.log(logging.DEBUG, 'Sending message to pingpong subprocess:\n' + msg)
+        try:
+            self.proc.stdin.write(msg)
+            self.proc.stdin.flush()
+        except IOError as e:
+            msg = 'Error writing data to pingpong subprocess: {0}\n'.format(e)
+            out = self._try_read_subprocess_error()
+            if out:
+                msg += 'Subprocess output:\n' + out
+            raise DAPPException(msg)
+
+    def recv_msg(self):
+        # throughout this method, we strip just the trailing newline
+        #  from proc.stdout, since we want to get precise lines (whitespace can be significant)
+        lines = []
+        line = ''
+        while line != 'STOP' and self.proc.poll() is None:
+            # strip just the trailing newline
+            line = self.proc.stdout.readline().decode('utf8')[:-1]
+            lines.append(line)
+        if self.proc.poll() is not None:
+            # process ended, but we still haven't found STOP
+            rest = self.proc.stdout.read().decode('utf8')[:-1].splitlines()
+            lines.extend(rest)
+
+        self.log(logging.DEBUG,
+            'Got message from pingpong subprocess:\n{0}'.format('\n'.join(lines)))
+        msg = self._message_from_start_stop_list(lines)
+        if msg is None:
+            return None
+
+        parsed_yaml = yaml.load(msg)
+        # TODO: check msg_type
+        return parsed_yaml
+
+    def _try_read_subprocess_error(self):
+        """A helper method that just tries to read the rest of subprocess output if
+        something goes wrong, so that we can form a better error message."""
+        try:
+            return self.proc.stdout.read().decode('utf8')
+        except:
+            return None
+
+
+class DAPPClient(DAPPCommunicator):
+    def __init__(self, listen_fd=None, write_fd=None, protocol_version=__version__, logger=None):
+        super(DAPPClient, self).__init__(protocol_version, logger)
+        # we want to write bytes in Python 3, so we use buffer for sys.stdin and sys.stdout
+        stdin = sys.stdin if six.PY2 else sys.stdin.buffer
+        stdout = sys.stdout if six.PY2 else sys.stdout.buffer
+        self.listen_fd = listen_fd or stdin
+        self.write_fd = write_fd or stdout
+
+    def send_msg(self, msg_type, ctxt=None, data=None):
+        # TODO: minor code duplication with the method in DAPPServer, maybe refactor
+        send_msg = {'msg_type': msg_type}
+        if data:
+            send_msg.update(data)
+        msg = self._compose_msg(ctxt, data=send_msg)
+        self.log(logging.DEBUG, 'Sending message to pingpong server:\n' + msg.decode('utf8'))
+        self.write_fd.write(msg)
+        self.write_fd.flush()
+
+    def recv_msg(self):
+        lines = []
+        line = ''
+        while line != 'STOP':
+            # strip just the trailing newline
+            line = self.listen_fd.readline().decode('utf8')[:-1]
+            lines.append(line)
+
+        msg = self._message_from_start_stop_list(lines)
+        if msg is None:
+            return None
+
+        parsed_yaml = yaml.load(msg)
+        # TODO: check msg_type
+        return parsed_yaml
+
+    def pingpong(self):
+        """A method that manages PingPong on client side;
+
+        The flow:
+        1) wait for DevAssistant to send a message of type "run"
+        2) call the "run" method (this is supposed to be subclassed in the actual
+           executable assistants)
+        3) sends a message of type "finished" with result of the execution
+        """
+        msg = self.recv_msg()
+        if msg['msg_type'] != 'run':
+            raise # TODO
+        run_result = self.run(msg['ctxt'])
+        if not isinstance(run_result, tuple) or len(run_result) != 3:
+            raise # TODO
+        # TODO we should send the resulting context, too
+        self.send_msg(msg_type='finished', ctxt=run_result[2],
+            data={'lres': run_result[0], 'res': run_result[1]})
+
+    def call_command(self, command_type, command_input, ctxt):
+        """Call a DevAssistant command.
+
+        Args:
+            command_type: DevAssistant command type
+            command_input: command input for the called command
+            ctxt: the global context
+
+        Returns:
+            3-tuple - logical result of command, result of command, new global context (might
+            have been modified by the command)
+        """
+        self.send_msg(msg_type='call_command', ctxt=ctxt,
+            data={'command_type': command_type, 'command_input': command_input})
+        response = self.recv_msg()
+        return response['lres'], response['res'], response['ctxt']
